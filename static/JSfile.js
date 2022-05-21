bbf54bf1bc9543df784b4f9ec0927e40ac01ae29
talyellin23 <yellin.tal.rish@gmail.com>
2022-05-21 17:08:32 +0300

    Final2

--- static/JSfile.js
+++ static/JSfile.js
@@ -1,20 +1,20 @@
 const activePage = window.location.pathname;
 
-const navLinks = document.querySelectorAll('nav a').forEach(link => {    
-  if(link.href.includes(`${activePage}`)){
+const navLinks = document.querySelectorAll('nav a').forEach(link => {
+  if (link.href.includes(`${activePage}`)) {
     link.classList.add('active');
   }
 });
 
-const ReadMoreButton= document.querySelector('.readMoreButton');
-const text =document.querySelector('.text');
+const ReadMoreButton = document.querySelector('.readMoreButton');
+const text = document.querySelector('.text');
 
-ReadMoreButton.addEventListener('click',(e)=>{
+ReadMoreButton.addEventListener('click', (e) => {
   text.classList.toggle('show-more');
-  if(ReadMoreButton.innerText== "Click hear to read more"){
-    ReadMoreButton.innerText= "Click hear to read less";
+  if (ReadMoreButton.innerText == "Click hear to read more") {
+    ReadMoreButton.innerText = "Click hear to read less";
   }
-  else{
-    ReadMoreButton.innerText= "Click hear to read more";
+  else {
+    ReadMoreButton.innerText = "Click hear to read more";
   }
 })
